3435cbd34f8090ce38206cabda323c5712dc38bb
harshgupta0 <hg09032@gmail.com>
2023-10-06 22:07:47 +0530

    added delete functionality

--- lib/Screens/TodoListPage.dart
+++ lib/Screens/TodoListPage.dart
@@ -44,12 +44,32 @@ class _TodoPageState extends State<TodoPage> {
                 itemCount: items.length,
                 itemBuilder: (context, index) {
                   final item = items[index] as Map;
+                  final id=item['_id'] as String;
                   return ListTile(
                     leading: CircleAvatar(
                       child: Text('${index + 1}'),
                     ),
                     title: Text(item['title']),
                     subtitle: Text(item['description']),
+                    trailing: PopupMenuButton(
+                      onSelected: (value){
+                        if(value=='edit'){
+                        //open and edit
+                        }else if(value=='delete'){
+                          //delete and remove the item
+                        }
+                      },
+                      itemBuilder: (context){
+                        return [
+                          PopupMenuItem(child: Text('Edit'),
+                            value: 'edit',
+                          ),
+                          PopupMenuItem(child: Text('Delete'),
+                            value: 'delete',
+                          ),
+                        ];
+                      },
+                    ),
                   );
                 }
                 )
@@ -57,7 +77,20 @@ class _TodoPageState extends State<TodoPage> {
       ),
     ));
   }
+ Future<void> DeleteById( String id)async{
+     final url ='https://api.nstack.in/v1/todos/$id';
+     final uri = Uri.parse(url);
+     final response=await http.delete(uri);
+     if(response.statusCode==200){
+       final filtered =items.where((element) => element['_id']!=id).toList();
+       setState(() {
+         items=filtered;
+       });
+     }else{
+       ShowFailureMessage('Erorr Deleting');
+     }
 
+ }
   Future<void> FetchTodo() async {
     final url = 'https://api.nstack.in/v1/todos?page=1&limit=10';
     final uri = Uri.parse(url);
@@ -74,5 +107,21 @@ class _TodoPageState extends State<TodoPage> {
     });
     print(response.body);
     print(response.statusCode);
+  }
+  void ShowFailureMessage(String Message) {
+    final snackBar = SnackBar(
+      content: Container(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(Message,style: TextStyle(fontSize: 22,color: Colors.white),),
+            Icon(Icons.close),
+
+          ],
+        ),
+      ),
+      backgroundColor: Colors.red.shade200,
+    );
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
   }
 }
