c3b4cb1a4749257084fbbe8f51343d78ad1243ed
harshgupta0 <hg09032@gmail.com>
2023-10-06 21:33:11 +0530

    added push and get functionality

--- lib/Screens/TodoListPage.dart
+++ lib/Screens/TodoListPage.dart
@@ -11,13 +11,15 @@ class TodoPage extends StatefulWidget {
 }
 
 class _TodoPageState extends State<TodoPage> {
-  List items=[];
+  bool isLoading = true;
+  List items = [];
   @override
   void initState() {
     // TODO: implement initState
     FetchTodo();
     super.initState();
   }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -28,30 +30,49 @@ class _TodoPageState extends State<TodoPage> {
       ),
       floatingActionButton: FloatingActionButton.extended(
           onPressed: () {
-            Navigator.pushNamed(context,'/AddTodo');
-          }, label: Text('Add Todo')),
-      body: Container(
-        child: SingleChildScrollView(
-          child: Column(
-            children: [],
-          ),
+            Navigator.pushNamed(context, '/AddTodo');
+          },
+          label: Text('Add Todo')),
+      body: Visibility(
+        visible: isLoading,
+        child: Center(
+          child: CircularProgressIndicator(),
+        ),
+        replacement: RefreshIndicator(
+            onRefresh: FetchTodo,
+            child: ListView.builder(
+                itemCount: items.length,
+                itemBuilder: (context, index) {
+                  final item = items[index] as Map;
+                  return ListTile(
+                    leading: CircleAvatar(
+                      child: Text('${index + 1}'),
+                    ),
+                    title: Text(item['title']),
+                    subtitle: Text(item['description']),
+                  );
+                }
+                )
         ),
       ),
     ));
   }
-  Future<void>FetchTodo()async{
-    final url ='https://api.nstack.in/v1/todos?page=1&limit=10';
-    final uri =Uri.parse(url);
+
+  Future<void> FetchTodo() async {
+    final url = 'https://api.nstack.in/v1/todos?page=1&limit=10';
+    final uri = Uri.parse(url);
     final response = await http.get(uri);
-    if(response.statusCode==200){
-      final json= jsonDecode(response.body) as Map;
-      final result= json["items"] as  List;
+    if (response.statusCode == 200) {
+      final json = jsonDecode(response.body) as Map;
+      final result = json["items"] as List;
       setState(() {
-        items=result;
+        items = result;
       });
     }
+    setState(() {
+      isLoading = false;
+    });
     print(response.body);
     print(response.statusCode);
-
   }
 }
